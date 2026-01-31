695ea7041ce266e79f41902ca49993408b91bbbf
Kaiser <hichamlaaouni52@gmail.com>
2026-01-31 22:33:41 +0100

    feat: base del juego
    Movimiento del jugador jugador además de añadir los bloques que lo protegen y crear animaciones de los enemigos.

---
+++ Assets/_Project/Script/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody2D myRb;
+    Vector2 moveInput;
+    
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        moveInput = value.ReadValue<Vector2>();
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, 0f);
+        myRb.linearVelocity = playerVelocity;
+    }
+}
