e50ebe2789964b4c75056bd15eb2b3100fc1f4b7
Kaiser <hichamlaaouni52@gmail.com>
2026-02-04 00:23:29 +0100

    fix: if sobrante
    Al final tome otro enfoque y por lo tanto ya no es necesario

--- Assets/_Project/Script/EnemyManager.cs
+++ Assets/_Project/Script/EnemyManager.cs
@@ -14,14 +14,7 @@ public class EnemyManager : MonoBehaviour
 
     void Update()
     {
-        if (isExplosioning)
-        {
-            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-            {
-                Destroy(gameObject);
-                isExplosioning = false;
-            }
-        }
+       
     }
 
     void OnCollisionEnter2D(Collision2D collision)
